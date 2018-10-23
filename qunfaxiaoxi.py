e6aa80a77b5e1b88e43d11402ea9a09d329b30db
wg18618262234 <wg18618262234@outlook.com>
2018-10-23 18:45:50 +0800

    Initial commit

---
+++ qunfaxiaoxi.py
@@ -0,0 +1,14 @@
+import itchat, time
+
+itchat.auto_login(True)  ###登录，扫码，相当于登录微信网页版
+itchat.run()  ###loop，监听收到消息事件
+
+WANT_TO_SAY = '祝%s狗年旺旺，身体健康！！'
+
+friendList = itchat.get_friends(update=True)[1:]
+i = 0
+for friend in friendList:
+    i += 1
+    print('第%d个    ' % (i), WANT_TO_SAY % (friend['DisplayName'] or friend['NickName']))
+    # itchat.send(WANT_TO_SAY % (friend['DisplayName'] or friend['NickName']))
+    time.sleep(.3)
